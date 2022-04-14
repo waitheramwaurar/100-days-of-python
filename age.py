b11a3788ac3f4ffe3befc49c70b461e12648de53
CeraMwaura <>
2022-04-14 09:10:52 +0000

    Age calculator

---
+++ age.py
@@ -0,0 +1,8 @@
+age = input("What is your current age?")
+remaining_yrs = 90 - int(age)
+
+months = remaining_yrs * 12
+weeks = remaining_yrs * 52
+days = remaining_yrs * 365
+
+print(f"You have {days} days, {weeks} weeks, {months} months left.")
